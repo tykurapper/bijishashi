1f11c048b03a36acdfed517f3111826479f63318
Tyku <tuananh.banhdauxanh@gmail.com>
2017-04-24 22:26:24 +0700

    Comment + Giao dien + Edit image

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,5 @@ class Image < ActiveRecord::Base
     mount_uploader :file, ImageUploader
     belongs_to :user
     belongs_to :category
+    has_many :image_comment, -> { order "created_at DESC"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415125447) do
+ActiveRecord::Schema.define(version: 20170424144528) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "image_comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "image_id"
+    t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
