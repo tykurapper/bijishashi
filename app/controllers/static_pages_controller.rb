b49aec97637da75f37d9ec2f891c8f87583afdc4
nguyenxuanthai <xuanthainbk@gmail.com>
2017-05-06 16:37:49 +0700

    Done show image

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+  	if user_signed_in?
+  		following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = #{current_user.id}"
+		@image = Image.where("user_id IN (#{following_ids})OR user_id = #{current_user.id}").order("created_at desc")
+	end		
   end
 end
