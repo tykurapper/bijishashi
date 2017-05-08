660237f94934004099aacd5bb6bed0b32b83076f
Tyku <tuananh.banhdauxanh@gmail.com>
2017-05-08 23:44:40 +0700

    sprint 3 chinh home

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426132357) do
-
+ActiveRecord::Schema.define(version: 20170504145915) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +36,16 @@ ActiveRecord::Schema.define(version: 20170426132357) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["image_id"], name: "index_likes_on_image_id"
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
+
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
@@ -47,15 +56,6 @@ ActiveRecord::Schema.define(version: 20170426132357) do
   add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
-  create_table "likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "likes", ["image_id"], name: "index_likes_on_image_id"
-  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
